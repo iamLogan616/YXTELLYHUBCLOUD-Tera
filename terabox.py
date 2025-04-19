33c97b20fa4c0f19249f97db0385af96f5cef63b
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-04-19 12:43:33 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -197,7 +197,7 @@ async def start_command(client: Client, message: Message):
     repo69 = InlineKeyboardButton("ʀᴇᴘᴏ 🌐", url="https://github.com/Hrishi2861/Terabox-Downloader-Bot")
     reply_markup = InlineKeyboardMarkup([[join_button, developer_button], [repo69]])
     final_msg = "🌟 ɪ ᴀᴍ ᴀ ᴛᴇʀᴀʙᴏx ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ.\n\nYou already have a valid token!"
-    video_file_id = "/app/Jet-Mirror.mp4"
+    video_file_id = ""
     if len(message.command) > 1 and len(message.command[1]) == 36:
         token = message.command[1]
         user_id = message.from_user.id
